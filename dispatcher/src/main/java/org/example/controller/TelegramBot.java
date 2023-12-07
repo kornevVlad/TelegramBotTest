116e8506bc3543b09d3e971faa4954724cac5fc0
kornevVlad <vlad1990@yandex.ru>
2023-12-07 13:13:28 +0300

    Проведено нагрузочное тестирование кода. Файл test_bot.csv

--- dispatcher/src/main/java/org/example/controller/TelegramBot.java
+++ dispatcher/src/main/java/org/example/controller/TelegramBot.java
@@ -3,14 +3,13 @@ package org.example.controller;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.generics.Webhook;
+
 
 import javax.annotation.PostConstruct;
 
